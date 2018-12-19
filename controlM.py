e5c8d00bce1aeea3d35eb8214538ad9d20abb958
James <james.gong@accesshq.com>
2018-12-20 10:40:05 +1100

    minor change on coding structure, add the settings.local.py

--- controlM.py
+++ controlM.py
@@ -1,19 +1,23 @@
 import sys
 import requests
 import json
+from settings.local import BASE_URL, USERNAME, PASSWORD, CTM
 
-BASE_URL = "https://localhost:8443/automation-api"
-
-def login(base_url=BASE_URL, username="workbench", password="workbench"):
+def login(base_url=BASE_URL, username=USERNAME, password=PASSWORD):
     # define the login endpoint
     login_url = base_url + '/session/login'
 
-    # forming the request body and headers
-    body = {"password": password, "username": username}
+    # define the headers
     headers = {'content-type': 'application/json'}
+
+    # forming the request body
+    body = { 
+        "username": username,
+        "password": password 
+    }
     
     # get the response and change into the json format
-    response = requests.post(login_url, json=body, verify=False, headers=headers)
+    response = requests.post(login_url, json=body, headers=headers, verify=False)
     json_response = response.json()
 
     print("This is the login token", json_response['token'])
@@ -45,6 +49,18 @@ def run_jobs(token, jobfile, base_url=BASE_URL):
     response = requests.post(run_job_url, headers=headers, files=file_data, verify=False)
     return response
 
+def run_order_jobs(token, folder, ctm_name=CTM, base_url=BASE_URL):
+
+    run_order_job_url = base_url = '/run/order'
+    body = {
+        "ctm": ctm_name,
+        "folder": folder
+    }
+    headers = json.loads('{"Authorization": "Bearer ' + token + '"}')
+    response = requests.post(run_order_job_url, headers=headers, json=body, verify=False)
+
+    return response
+
 def main():
     
     JOB_FILE = sys.argv[1]
@@ -56,9 +72,9 @@ def main():
     # Create the jobs
     # response = create_jobs(token=token, jobfile=JOB_FILE)
 
-    response = run_jobs(token=token, jobfile=JOB_FILE)
+    # response = run_jobs(token=token, jobfile=JOB_FILE)
 
-    print("###", response, response.text)
+    # print("###", response, response.text)
 
 
 if __name__ == '__main__':
---
+++ settings/local.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://localhost:8443/automation-api"
+USERNAME = "workbench"
+PASSWORD = "workbench"
+CTM = "workbench"
