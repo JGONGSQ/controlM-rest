981df44f50f500428a235d528f91960e85fe237d
James <james.gong@accesshq.com>
2019-01-09 10:03:37 +1100

    Refactor and add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils import setup
+
+setup(
+    name='controlM-rest',
+    version='0.1',
+    packages=['controlm-rest-api',],
+    license='Creative Commons Attribution-Noncommerical-Share Alike license',
+    long_description=open('README.txt').read(),
+)
