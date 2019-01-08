af6cbdb92f61cce37f50a6c775ea15072d903fe7
James <james.gong@accesshq.com>
2019-01-09 10:16:52 +1100

    Adding the license txt file and add more lines in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='controlM-rest',
     version='0.1',
     packages=['controlm-rest-api',],
-    license='Creative Commons Attribution-Noncommerical-Share Alike license',
-    long_description=open('README.txt').read(),
+    author='James Gong',
+    long_description=open('README.md').read(),
+    keywords=['ControlM', 'REST-api'],
+    python_requires='>=2.7',
+    install_requires=['requests >= 2.8, < 3.0.0a0'],
 )
