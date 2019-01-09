41491903919261bcf517db297c87f7a110428679
James <james.gong@accesshq.com>
2019-01-09 12:12:26 +1100

    Adding more lines to the .gitignore

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='controlM-rest',
     version='0.1',
-    packages=['controlm-rest-api',],
-    author='James Gong',
-    long_description=open('README.md').read(),
+    # packages=['controlm-rest-api',],
+    packages=find_packages(),
+    long_description=open('README.txt').read(),
     keywords=['ControlM', 'REST-api'],
-    python_requires='>=2.7',
+    # python_requires='>=2.7',
     install_requires=['requests >= 2.8, < 3.0.0a0'],
 )
