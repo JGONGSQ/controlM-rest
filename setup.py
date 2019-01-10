2ac04d5b8a07ae10c4f071192837a25b23c16057
James <james.gong@accesshq.com>
2019-01-10 13:35:56 +1100

    change the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='controlM-rest',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     long_description=open('README.txt').read(),
     keywords=['ControlM', 'REST-api'],
