d112d6153596b5627c816b87677b201c5aa8e207
James <james.gong@accesshq.com>
2019-01-11 15:57:52 +1100

    Minor changes on the structure of the scripts

--- controlm-rest-api/ctrlM.py
+++ controlm-rest-api/ctrlM.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import json
+
 from settings.local import BASE_URL, USERNAME, PASSWORD
 from library.core import CoreAPIs 
 
@@ -10,12 +11,28 @@ def main():
     authentication_str = 'auth'
     extra_var_str = 'extra_var'
 
+    action_login = 'login'
+    action_run = 'run'
+    action_deploy = 'deploy'
+
     parser = argparse.ArgumentParser(
-        description="Start of the ControlM REST API package"
+        description="ControlM REST API package, it uses the REST api to connect the ControlM Server"
         )
-    parser.add_argument(action_str, help="The action take for the script, e.g. deploy or run")
-    parser.add_argument("-"+authentication_str, nargs="?", help='The authentication dict including {"username":"username", "password":"password", "base_url":"base_url"}')    
-    parser.add_argument("-"+extra_var_str, nargs="?", help="The extra parameters would be needed for the action, could be a filepath or extra extra variables")
+    parser.add_argument(
+        action_str, 
+        choices=[action_login, action_deploy, action_run], 
+        help="The action take for the script, e.g. deploy or run"
+        )
+    parser.add_argument(
+        "-"+authentication_str, 
+        nargs="?", 
+        help='The authentication dict including {"username":"username", "password":"password", "base_url":"base_url"}'
+        )    
+    parser.add_argument(
+        "-"+extra_var_str, 
+        nargs="?", 
+        help="The extra parameters would be needed for the action, could be a filepath or extra extra variables"
+        )
     args = parser.parse_args()
 
     authentication_json = getattr(args, authentication_str)
@@ -26,16 +43,16 @@ def main():
         core_apis = CoreAPIs(auth_dict['username'], auth_dict['password'], auth_dict['base_url'])
 
     # login to the system
-    if args.action == 'login':
+    if args.action == action_login:
         response = core_apis.login()
 
     # deploy jobs
-    if args.action == 'deploy': 
+    if args.action == action_deploy: 
         job_file = getattr(args, extra_var_str)
         response = core_apis.deploy_jobs(job_file)
 
     # run order the jobs
-    if args.action == 'run':
+    if args.action == action_run:
         extra_var = getattr(args, extra_var_str)
         if not extra_var:
             extra_var = {"orderDate": "20181212"}
