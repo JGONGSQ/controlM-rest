8a08cda26f70b70ad3cd01346d51e039fd495e12
James <james.gong@accesshq.com>
2019-01-16 11:22:28 +1100

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='controlM-rest',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     long_description=open('README.txt').read(),
     keywords=['ControlM', 'REST-api'],
